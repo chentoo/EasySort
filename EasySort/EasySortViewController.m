68d31954d3613f32672b20bcbb1fe452b03639cf
chentoo <chentoo@gmail.com>
2013-11-26 10:52:25 +0800

    1 step

--- EasySort/EasySortViewController.m
+++ EasySort/EasySortViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "EasySortViewController.h"
+#import "SortAlgorithm.h"
 
 @interface EasySortViewController ()
 
@@ -18,6 +19,8 @@
 {
     [super viewDidLoad];
     NSLog(@"hehe");
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
--- EasySort/SortAlgorithm.m
+++ EasySort/SortAlgorithm.m
@@ -10,4 +10,25 @@
 
 @implementation SortAlgorithm
 
+
+/**
+ *  Bubble sort 1.
+ *
+ *  @param nums 数组中的数据用array方式保存，排序时，更改相应位置的元素的值。
+ */
++ (void)BubbleSort1:(NSMutableArray *)nums
+{
+    NSInteger i, j;
+    for (i = 0; i < nums.count; i++)
+    {
+        for (j = 1; j < nums.count - 1; j++)
+        {
+            if ([nums[j - 1] floatValue] > [nums[j] floatValue])
+            {
+                [nums exchangeObjectAtIndex:j-1 withObjectAtIndex:j];
+            }
+        }
+    }
+}
+
 @end
