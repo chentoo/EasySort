abef7c8a09a5d2603f62d26e5b78b9e861d6d996
chentoo <chentoo@gmail.com>
2013-11-26 10:28:29 +0800

    add sort algorithm file

---
+++ EasySort/SortAlgorithm.h
@@ -0,0 +1,13 @@
+//
+//  SortAlgorithm.h
+//  EasySort
+//
+//  Created by Summer on 26/11/13.
+//  Copyright (c) 2013 kodak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SortAlgorithm : NSObject
+
+@end
---
+++ EasySort/SortAlgorithm.m
@@ -0,0 +1,13 @@
+//
+//  SortAlgorithm.m
+//  EasySort
+//
+//  Created by Summer on 26/11/13.
+//  Copyright (c) 2013 kodak. All rights reserved.
+//
+
+#import "SortAlgorithm.h"
+
+@implementation SortAlgorithm
+
+@end
